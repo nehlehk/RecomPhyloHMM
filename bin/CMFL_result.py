d4fe1ff09a48dbcac82c3a95214c9da0cd8011cc
kargarfard870@gmail.com <F@temeh870>
2021-03-10 13:44:29 +1100

    add rmse calc here

--- bin/CMFL_result.py
+++ bin/CMFL_result.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 from dendropy import Tree
 import dendropy
+from sklearn.metrics import mean_squared_error
+import csv
 
 
 # **********************************************************************************************************************
@@ -61,6 +63,50 @@ def CFML_resultFig(tree,CFMLData):
     # plt.show()
     plt.savefig("CFML_Recombination.jpeg")
 # **********************************************************************************************************************
+def real_recombination(recomLog):
+    realData = np.zeros((alignment_len, nodes_number))
+    df = pd.read_csv(recomLog,sep='\t', engine='python')
+    # print(df)
+    recom = df.loc[df['status'] != 'clonal']
+    recom = recom.reset_index(drop=True)
+    for i in range(len(recom)):
+        s = recom['start'][i]
+        t = recom['end'][i]
+        # print(recom['nodes'][i])
+        nodes = nodes_separation(recom['nodes'][i])
+        # print(nodes)
+        for i in range(len(nodes)):
+            mynode = int(give_taxon_index(tree, nodes[i]))
+            realData[s:t, mynode] = 1
+
+    return realData
+# **********************************************************************************************************************
+def nodes_separation(nodes):
+  nodes = str(nodes)
+  nodes = nodes[1:-1]
+  mynodes = nodes.split(",")
+  return mynodes
+# **********************************************************************************************************************
+def give_taxon_index(tree,taxa):
+    node_mapping = np.zeros((nodes_number,2))
+    i = 0
+    for node in tree.postorder_node_iter():
+      if node.is_leaf():
+        node_mapping[i][0] = int(str(node.taxon.label))
+      else:
+        node_mapping[i][0] = int(str(node.index))
+      node_mapping[i][1] = int(str(node.index))
+      i = i+1
+
+    for i in range(len(node_mapping)):
+      if int(node_mapping[i][0]) == int(taxa):
+        return node_mapping[i][1]
+# **********************************************************************************************************************
+def write_rmse_CFML(rmse_real_CFML):
+    with open('rmse_CFML.csv', mode='w') as rmse_file:
+        rmse_writer = csv.writer(rmse_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        rmse_writer.writerow([rmse_real_CFML])
+# **********************************************************************************************************************
 
 
 if __name__ == "__main__":
@@ -69,12 +115,14 @@ if __name__ == "__main__":
     parser.add_argument('-a', "--alignmentFile", type=str, required= True , help='fasta file')
     parser.add_argument('-c', "--cfmlFile", type=str, help='cfmlFile')
     parser.add_argument('-ct', "--cfmltreefile", type=str, help='cfmltreefile')
+    parser.add_argument('-l', "--recomlogFile", type=str, help='recombination log file')
     args = parser.parse_args()
 
     tree_path = args.treeFile
     cfml_path = args.cfmlFile
     cfml_tree = args.cfmltreefile
     genomefile = args.alignmentFile
+    recomLog = args.recomlogFile
 
 
     tree = Tree.get_from_path(tree_path, 'newick')
@@ -86,4 +134,9 @@ if __name__ == "__main__":
     alignment_len = alignment.sequence_size
 
     CFMLData = CFML_recombination(cfml_path)
+    realData = real_recombination(recomLog)
     CFML_resultFig(cfml_tree, CFMLData)
+
+    rmse_real_CFML = mean_squared_error(realData, CFMLData, squared=False)
+
+    write_rmse_CFML(rmse_real_CFML)
