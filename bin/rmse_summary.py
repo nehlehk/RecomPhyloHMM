3c11a2bfc5b9f6780c1ed616e42c429409a0d8a6
kargarfard870@gmail.com <F@temeh870>
2021-03-11 12:03:34 +1100

    modify plot for diffrent nu and prob

--- bin/rmse_summary.py
+++ bin/rmse_summary.py
@@ -2,27 +2,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import argparse
 import seaborn as sns
+import numpy as np
 # from pycallgraph import PyCallGraph
 # from pycallgraph.output import GraphvizOutput
 
 parser = argparse.ArgumentParser(description='''You did not specify any parameters.''')
-parser.add_argument('-f', "--rmseFiles", type=str, help='rmseFiles')
+parser.add_argument('-p', "--phylohmm", type=str, help='rmse_phylohmm')
+parser.add_argument('-c', "--CFML", type=str, help='rmse_CFML')
 args = parser.parse_args()
 
 
-rmse_path = args.rmseFiles
+rmse_phylohmm = args.phylohmm
+rmse_CFML = args.CFML
 
 # with PyCallGraph(output=GraphvizOutput()):
-# rmse_path = '/home/nehleh/Desktop/rmse30.csv'
+# rmse_phylohmm = '/home/nehleh/PhyloCode/Result/test/results/Summary_Results/rmse_phylohmm.csv'
+# rmse_CFML = '/home/nehleh/PhyloCode/Result/test/results/Summary_Results/rmse_CFML.csv'
+
+
+f = open(rmse_phylohmm, "r")
+df = pd.read_csv(f, sep=';', names=['nu', 'prob','Phylohmm'], header=None)
+# print(df)
+
+
+f2 = open(rmse_CFML, "r")
+df2 = pd.read_csv(f2, sep=';', names=['cfml'], header=None)
+# print(df2)
 
 
 
-f = open(rmse_path, "r")
-df = pd.read_csv(f, sep=';', names=['nu', 'prob','Phylohmm','CFML'], header=None)
-print(df)
-# nu_value = df['nu'].unique()
-# prob_value = df['prob'].unique()
-# case_num = df['CFML'].nunique()
 
 
 # print(pd.melt(df))
@@ -32,20 +40,45 @@ print(df)
 # ax = sns.boxplot(data=new_df, x='variable', y='value', hue='nu')
 
 # sns.boxplot(x="variable", y="value", data=pd.melt(df))
+# ======================================================================================================================
+
+Pr = np.unique(df['prob'])
+Nu = np.unique(df['nu'])
+pr_num = len(Pr)
+nu_num = len(Nu)
+i = 1
+fig = plt.figure(figsize=(15,15))
+for n in Nu:
+    for p in Pr:
+        s_df = df.loc[df['nu'] == n]
+        temp = s_df.loc[s_df['prob'] == p]
+        new = pd.concat([temp['Phylohmm'],df2],axis=1)
+        ax1 = fig.add_subplot(pr_num, nu_num, i)
+        ax1 = sns.boxplot( data=new  )
+        ax1 = sns.stripplot(data=new,  jitter=True, dodge=True, marker='o', color=".1")
+        ax1.set_title('RMSE values when nu =' + str(n) + ' and prob =' + str(p) , fontsize=9)
+        plt.ylabel('RMSE')
+        i = i+1
 
 
 
-fig = plt.figure(figsize=(8,8))
-ax1 = fig.add_subplot(2, 1, 1)
-ax1 = sns.boxplot(x = 'nu', y="Phylohmm" ,data=df , hue = 'prob' )
-ax1 = sns.stripplot(x = 'nu', y="Phylohmm",data=df, hue='prob' , jitter=True, dodge=True, marker='o', color=".5")
-handles, labels = ax1.get_legend_handles_labels()
-l = plt.legend(handles[0:5], labels[0:5] , title= 'Probablity')
 
 
-ax2 = fig.add_subplot(2, 1, 2)
-ax2 = sns.boxplot(x = 'nu',y=df["CFML"] ,data=df  )
-# ax2 = sns.stripplot(x = 'nu', y="CFML",data=df , jitter=True, dodge=True, marker='o', color=".5")
+
+
+
+# ======================================================================================================================
+# fig = plt.figure(figsize=(8,8))
+# ax1 = fig.add_subplot(2, 1, 1)
+# ax1 = sns.boxplot(x = 'nu', y="Phylohmm" ,data=df , hue = 'prob' )
+# ax1 = sns.stripplot(x = 'nu', y="Phylohmm",data=df, hue='prob' , jitter=True, dodge=True, marker='o', color=".5")
+# handles, labels = ax1.get_legend_handles_labels()
+# l = plt.legend(handles[0:3], labels[0:3] , title= 'Probablity')
+#
+#
+# ax2 = fig.add_subplot(2, 1, 2)
+# ax2 = sns.boxplot(y=df2["cfml"] ,data=df2  )
+# ax2 = sns.stripplot(y="cfml",data=df2 , jitter=True, dodge=True, marker='o', color="0.1")
 # handles, labels = ax2.get_legend_handles_labels()
 # l = plt.legend(handles[0:5], labels[0:5] , title= 'Probablity')
 
